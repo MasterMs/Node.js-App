5b4f2f47a9b7ace4eba8bcea33a1e2ee5b78dbc3
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-01-27 01:14:30 -0500

    Update app.js

--- Main_Files/app.js
+++ Main_Files/app.js
@@ -1,28 +1,31 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+var mysql = require('mysql');
 
 //const hostname = '127.0.0.1';
 const hostname = 'localhost';
 const port = 8080;
+const database ={
+  'host': 'localhost',
+  'user': "root",
+  'password': "admin"
+}
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
+	res.setHeader('Content-Type', 'text/html');
+	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  myReadStream.pipe(res);
 });
-
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
-
-var mysql = require('mysql');
-
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin"
+  host: database['host'],
+  user: database['user'],
+  password: database['password']
 });
 
 con.connect(function(err) {
