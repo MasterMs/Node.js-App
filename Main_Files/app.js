fea1b576a641d57c59f7993dca719861122853a1
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-01-28 15:48:32 -0500

    Update app.js

--- Main_Files/app.js
+++ Main_Files/app.js
@@ -1,24 +1,27 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var mysql = require('mysql');
 var dotenv = require('dotenv');
+dotenv.config()
+
 
 //const hostname = '127.0.0.1';
 const hostname = 'localhost';
 const port = 8080;
 
-dotenv.config()
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
+const server = http.createServer((request, response) => {
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/html');
 	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  myReadStream.pipe(res);
+  
 });
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
