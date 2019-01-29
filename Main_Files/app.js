d93ae1febaa8baa9950bb4cb7c1e432a2c03be1d
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-01-29 00:18:50 -0500

    Update app.js

--- Main_Files/app.js
+++ Main_Files/app.js
@@ -1,3 +1,4 @@
+//imports
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
@@ -5,31 +6,36 @@ var mysql = require('mysql');
 var dotenv = require('dotenv');
 var express = require('express');
 
+//initialize express
 var app  = express();
 
+//initialize dotenv
 const result = dotenv.config();
 if (result.error){
   throw result.error;
 }
 
-// define hostname and port
+// initialize hostname and port
 const hostname = 'localhost';
 const port = 8080;
 
-
+//initialize database credentials and create a database connection
 var db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
 });
 
+// if error throw error else, connect to database and log connection
 db.connect(function(err) {
   if (err) {throw err;}
   console.log(`MySQL Database connected @ ${process.env.DB_HOST}:${process.env.DB_PORT}`);
 });
 
+//intialize static files 
 app.use(express.static('public'));
 
+//create server connection and log it
 app.listen(port, hostname, () => {
   console.log(`Server running @ ${hostname}:${port}/`);
 });
